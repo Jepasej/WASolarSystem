673f0e39af581df22ac60956f8cccab31b39b2c6
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-09-16 19:57:23 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using WASolarSystem;
+using WASolarSystem.View;
+using WASolarSystem.ViewModel;
+
+namespace WASolarSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow m = new();
+            m.DataContext = new MainWindowViewModel();
+            m.Show();
+        }
+        
+    }
+
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WASolarSystem.ViewModel
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        private BaseViewModel _currentViewModel;
+
+        public BaseViewModel CurrentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel = value;
+            }
+        }
+        //to hold our generated viewmodels, as to not new them every time.
+        private List<BaseViewModel> _viewModelList;
+
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new SolarSystemViewModel();
+            AddVMToList(CurrentViewModel);
+        }
+
+        private void AddVMToList(BaseViewModel vm)
+        {
+            if(_viewModelList == null)
+                _viewModelList = new List<BaseViewModel>();
+
+            if(vm != null) 
+                _viewModelList.Add(vm);
+        }
+    }
+}
